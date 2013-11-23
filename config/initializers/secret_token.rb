8f8c8358f156ccccae0d35c593f047c9ae1722c4
Roberto Riccio <RRiccio@Robertos-MacBook-Pro.local>
2013-11-23 16:37:01 -0200

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OMR::Application.config.secret_key_base = '24f7d75212a7dc01317d7e7390002d81a0e4eaab4f83eabe6ed3772497f25f224e8d9e12acd6b90d7c653a1991c4e6a5ec7fe396baf44585e3a0f606a731a2d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OMR::Application.config.session_store :cookie_store, key: '_OMR_session'
